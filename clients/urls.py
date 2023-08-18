b9727d5ba041de049ee5e8315ca5e2a44a4547c9
shlegel <sveta.shlegel@mail.ru>
2023-08-18 13:12:04 +0300

    completed Client CRUD

--- clients/urls.py
+++ clients/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 
-#from clients.views import
+from clients.views import ClientCreateView, ClientUpdateView, ClientListView, ClientDetailView, ClientDeleteView
 from clients.apps import ClientsConfig
 
 app_name = ClientsConfig.name
 
 urlpatterns = [
-    #path('', MailListView.as_view(), name='list'),
+    path('list/', ClientListView.as_view(), name='list'),
     #path('contacts/', contacts, name='contacts'),
-    #path('view/<int:pk>', MailDetailView.as_view(), name='view'),
-    #path('create/', MailCreateView.as_view(), name='create'),
-    #path('edit/<int:pk>', MailUpdateView.as_view(), name='edit'),
-    #path('delete/<int:pk>', MailDeleteView.as_view(), name='delete'),
+    path('view/<int:pk>', ClientDetailView.as_view(), name='view'),
+    path('create/', ClientCreateView.as_view(), name='create'),
+    path('edit/<int:pk>', ClientUpdateView.as_view(), name='edit'),
+    path('delete/<int:pk>', ClientDeleteView.as_view(), name='delete'),
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
+from django.urls import reverse
+from clients.models import Client
 
-# Create your views here.
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = ('first_name', 'last_name', 'nick', 'email')
+
+    def get_success_url(self):
+        return reverse('clients:list')
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    fields = ('first_name', 'last_name', 'nick', 'email')
+
+    def get_success_url(self):
+        return reverse('clients:view', args=[self.kwargs.get('pk')])
+
+
+class ClientListView(ListView):
+    model = Client
+
+
+class ClientDetailView(DetailView):
+    model = Client
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+
+    def get_success_url(self):
+        return reverse('clients:list')
+
